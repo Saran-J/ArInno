637ada5e46d4a03626711e7bdd34e44ee0f6a06e
Saran Jantara-amornporn <saran.j2108@gmail.com>
2019-09-15 00:47:03 +0700

    Initial Commit

---
+++ ArInnoTests/ArInnoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ArInnoTests.swift
+//  ArInnoTests
+//
+//  Created by KTB_User on 15/9/2562 BE.
+//  Copyright © 2562 KTB_User. All rights reserved.
+//
+
+import XCTest
+@testable import ArInno
+
+class ArInnoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
